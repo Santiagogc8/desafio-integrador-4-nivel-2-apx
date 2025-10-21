23c621321569978116fb51396c4ea9fa4635704b
Santiago Guzman <guzmansantiago833@gmail.com>
2025-10-20 23:57:20 -0500

    first commit

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/users', (req, res)=>{
     res.json({
