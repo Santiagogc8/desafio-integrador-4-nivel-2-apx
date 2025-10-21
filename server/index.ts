25b1946c42faa887238ac48cb3e805d44835e287
Santiago Guzman <guzmansantiago833@gmail.com>
2025-10-20 22:28:09 -0500

    first commit

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.get('/users', (req, res)=>{
+    res.json({
+        message: "todos los users"
+    })
+})
+
 // Determinamos la ruta absoluta a la carpeta 'dist' del frontend
 // __dirname es 'desafio-integrador-4/server'. Subimos (..) y entramos a 'client/dist'
 const staticPath = path.join(__dirname, '..', '..', 'client', 'dist');
